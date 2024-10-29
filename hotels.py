6d432f45299706af3e725c65acdb834503c3dc24
Лев <levsavinov07@gmail.com>
2024-10-29 12:47:21 +0300

    Add file

--- hotels.py
+++ hotels.py
@@ -1,10 +1,17 @@
-from fastapi import Query, Body, APIRouter
+from fastapi import Query, APIRouter, Body
+
+from Schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix  = "/hotels", tags = ["Отели"])
 
 hotels = [
     {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Dubai", "name": "dubai"}
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
 
 @router.get(
@@ -15,68 +22,86 @@ hotels = [
 def get_hotels(
         id:int | None = Query(None, description = "Айдишник"),
         title: str | None = Query(None, description = "Название отеля"),
+        page:int | None = Query(None, description = "Номер страницы"),
+        per_page:int | None = Query(None, description = "Количество отелей на страницу"),
 ):
     hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
+    if not id and not title and not per_page:
+        hotels_ = hotels
+    elif not id and not title and per_page and not page:
+        for i in range(per_page):
+            for hotel in hotels:
+                if hotel["id"] == i+1:
+                    hotels_.append(hotel)
+    elif not id and not title and per_page and page:
+        j = (page - 1) * per_page + 1
+        for i in range(j, per_page*page+1):
+            for hotel in hotels:
+                if hotel["id"] == i:
+                    hotels_.append(hotel)
+    else:
+        for hotel in hotels:
+            if id and hotel["id"] != id:
+                continue
+            if title and hotel["title"] != title:
+                continue
+            hotels_.append(hotel)
+
     return hotels_
+
 @router.put(
     "/{hotel_id}",
     summary = "Полное изменение отелей",
     description = "<h1>Введите айдишник отеля, который хотите изменить и все данные для него<h1>"
 )
-def change_info(
-        hotel_id: int,
-        title: str = Body(embed = True),
-        name: str = Body(embed = True)
-):
+def change_info(hotel_id: int, hotel_data: Hotel):
     global hotels
-    hotels[hotel_id-1]["title"] = title
-    hotels[hotel_id-1]["name"] = name
+    hotels[hotel_id-1]["title"] = hotel_data.title
+    hotels[hotel_id-1]["name"] = hotel_data.name
     return {"status":"OK"}
-
-@router.patch(
-    "/{hotel_id}",
-    summary = "Частичное изменение отелей",
-    description = "<h1>Введите айдишник отеля, который хотите изменить и данные, которые хотите изменить<h1>"
-)
-def partially_edit_hotel(
-        hotel_id: int,
-        title: str | None = Body(None),
-        name: str | None = Body(None),
-):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    if title:
-        hotel["title"] = title
-    if name:
-        hotel["name"] = name
-    return {"status": "OK"}
-
-
-
-#body, request body
 
 @router.post(
     "",
     summary = "Добавление отелей",
     description = "<h1> Введите данные отеля, который хотите добавить <h1>"
 )
-def create_hotel(
-        title: str = Body(embed = True),
+def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1":{"summary": "Сочи", "value":{
+            "title": "Отель Сочи 5 звезд у моря",
+            "name": "sochi_u_morya",
+        }},
+    "2":{"summary": "Дубай", "value":{
+            "title": "Отель Дубай у фонтана",
+            "name": "dubai_fontain",
+        }}
+})
 ):
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
-        "title": title
+        "title": hotel_data.title,
+        "name": hotel_data.name,
     })
     return {"status": "OK"}
 #Body позволяет уйти от Query параметров (они не подходят для фунции - post,
 #потому что отображаются в пути, в котором не должно быть информации о самом отеле)
+
+@router.patch(
+    "/{hotel_id}",
+    summary = "Частичное изменение отелей",
+    description = "<h1>Введите айдишник отеля, который хотите изменить и данные, которые хотите изменить<h1>"
+)
+def partially_edit_hotel(hotel_id: int, hotel_data: HotelPATCH):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if hotel_data.title:
+        hotel["title"] = hotel_data.title
+    if hotel_data.name:
+        hotel["name"] = hotel_data.name
+    return {"status": "OK"}
+
+#body, request body
+
 @router.delete(
     "/{hotel_id}",
     summary = "Удаление отелей",
