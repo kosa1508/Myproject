11df9e463da977a5c9b6d4399342be7dcc43d690
Лев <levsavinov07@gmail.com>
2025-01-06 22:00:23 +0300

    Add file

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,27 +1,59 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Response, Request
 
-from passlib.context import CryptContext
-
-from src.Schemas.users import UserRequestAdd, UserAdd
-from src.database import async_session_maker
 from src.repositories.users import UsersRepository
+from src.database import async_session_maker
+from src.Schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
+from src.services.cooki import cookie_parser
+from pydantic import EmailStr
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {"status": "OK"}
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd,
+        response:Response
+):
+    #hashed_password = pwd_context.hash(data.password)
+    #new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегестрирован")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
+
+    return {"status": "OK"}
+
+@router.get("/only_auth")
+async def login_user(
+    request: Request,
+    #access_token:str
+    email: EmailStr
+):
+    request = get_one_or_none(email)
+    #request = cookie_parser(access_token)
+    #access_token = request or None
+    async with async_session_maker() as session:
+        result = await UsersRepository(session).add(new_user_data)
+        await session.commit()
+
+
 
 
 
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,7 +1,41 @@
-from src.Schemas.users import User
+from sqlalchemy import select
+from pydantic import EmailStr
+
+from src.Schemas.users import User, UserWithHashedPassword
 from src.models.users import UsersOrm
 from src.repositories.base import BaseRepository
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
